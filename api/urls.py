9976d4d307202af908e9bc5ab158d7824c2e7f1f
pjo901018 <pjo901018@korea.ac.kr>
2017-03-26 21:17:52 +0900

    url format_suffic_patterns 추가

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 from .views import post_list,post_detail
+from rest_framework.urlpatterns import format_suffix_patterns
 
 
 urlpatterns = [
     url(r'^post/', post_list.as_view()),
     url(r'^post/(?P<pk>[0-9]+)/$', post_detail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
