65d3b3174773ad55aedf1ea917fe68a6f6a82fde
pjo901018 <pjo901018@korea.ac.kr>
2017-03-26 20:15:40 +0900

    database 교체

--- ALC_RestAPI/settings.py
+++ ALC_RestAPI/settings.py
@@ -81,8 +81,11 @@ WSGI_APPLICATION = 'ALC_RestAPI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'thealcapi$default',
+        'USER': 'thealcapi',
+        'PASSWORD': '',
+        'HOST': 'thealcapi.mysql.pythonanywhere-services.com',
     }
 }
 
@@ -109,9 +112,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
