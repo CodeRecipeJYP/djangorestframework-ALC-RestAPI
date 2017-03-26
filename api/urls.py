78a2662fb5353d3a5353a8838664b07ecc510d99
pjo901018 <pjo901018@korea.ac.kr>
2017-03-26 21:12:36 +0900

    Rest API Post CRUD기능 완성

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import post_api
+from .views import post_list,post_detail
 
 
 urlpatterns = [
-    url(r'^post/', post_api.as_view()),
-    ]
+    url(r'^post/', post_list.as_view()),
+    url(r'^post/(?P<pk>[0-9]+)/$', post_detail.as_view()),
+]
--- api/views.py
+++ api/views.py
@@ -7,9 +7,29 @@ from rest_framework.generics import GenericAPIView
 
 # Create your views here.
 
-class post_api(GenericAPIView, mixins.ListModelMixin):
+class post_list(GenericAPIView, mixins.ListModelMixin,
+               mixins.CreateModelMixin):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+class post_detail(GenericAPIView,
+                  mixins.DestroyModelMixin,
+                  mixins.UpdateModelMixin,
+                  mixins.RetrieveModelMixin):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
