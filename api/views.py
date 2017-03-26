0ebf050d22290603a0134e2d3f8b032720f657f6
pjo901018 <pjo901018@korea.ac.kr>
2017-03-26 21:40:19 +0900

    코드 간략화

--- api/views.py
+++ api/views.py
@@ -2,34 +2,30 @@ from django.shortcuts import render
 from rest_framework import mixins
 from .models import Post
 from .serializer import PostSerializer
-from rest_framework.generics import GenericAPIView
+from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
 
 
 # Create your views here.
 
-class post_list(GenericAPIView, mixins.ListModelMixin,
-               mixins.CreateModelMixin):
+class post_list(ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    #
+    # def get(self, request, *args, **kwargs):
+    #     return self.list(request, *args, **kwargs)
+    #
+    # def post(self, request, *args, **kwargs):
+    #     return self.create(request, *args, **kwargs)
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-class post_detail(GenericAPIView,
-                  mixins.DestroyModelMixin,
-                  mixins.UpdateModelMixin,
-                  mixins.RetrieveModelMixin):
+class post_detail(RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
+    #
+    # def get(self, request, *args, **kwargs):
+    #     return self.retrieve(request, *args, **kwargs)
+    #
+    # def put(self, request, *args, **kwargs):
+    #     return self.update(request, *args, **kwargs)
+    #
+    # def delete(self, request, *args, **kwargs):
+    #     return self.destroy(request, *args, **kwargs)
