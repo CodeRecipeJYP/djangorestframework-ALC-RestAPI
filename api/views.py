b9bbac5d9c2902c8b91c5857f476d1f39109c15d
pjo901018 <pjo901018@korea.ac.kr>
2017-03-28 20:52:08 +0900

    Post list 최신순으로 출력하게 변경

--- api/views.py
+++ api/views.py
@@ -8,9 +8,9 @@ from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIVi
 # Create your views here.
 
 class post_list(ListCreateAPIView):
-    queryset = Post.objects.all()
+    queryset = Post.objects.all().order_by('-uploaded_date')
     serializer_class = PostSerializer
 
 class post_detail(RetrieveUpdateDestroyAPIView):
-    queryset = Post.objects.all()
+    queryset = Post.objects.all().order_by('-uploaded_date')
     serializer_class = PostSerializer
